0a23ee4b873d9cb2e4c140b9129d82350a6c7030
Namgay Pelzang <02240319.cst@rub.edu.bt>
2026-03-29 15:03:15 -0700

    CSF Lab3

---
+++ Class_activity.py
@@ -0,0 +1,16 @@
+'''number=7
+result= "Even" if number % 2 == 0 else "odd"
+print(f"The number is {result}.")'''
+
+
+'''marks=60
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")'''
+    
+    
+'''x=10
+if x > 0:
+    print("x is positive")'''
+    
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,4 @@
 print("helloworld")
+x = "My name is Namgay Pelzang"
+age = int(input("Enter age: "))
+print(f"{x} and I am {age} years old")
